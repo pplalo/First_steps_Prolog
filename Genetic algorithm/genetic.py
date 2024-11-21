8b92feae32ec680a1972d70c398066f6eb1ad4b6
pplalo <jose.eduardo.castagno@gmail.com>
2024-11-21 00:14:07 -0600

    Refactor genetic algorithm to utilize put_major_value_coordinate and return list of selected machine pairs

--- Genetic algorithm/genetic.py
+++ Genetic algorithm/genetic.py
@@ -1,21 +1,23 @@
 import numpy as np
 import random
-from utils import fitness, reproduce, mutate, is_valid_child, get_random_zero_coordinate
+from utils import reproduce, mutate, put_major_value_coordinate
 
 def gen_algorithm(population):
     """
     Genetic algorithm to find the solution to the problem by mutating and reproducing solutions.
     population: Population of solutions.
     """
+    list_machines = []
     for i in range(len(population)):
         new_population = []
          
-        for j in range(len(population)):
-            if get_random_zero_coordinate(population) is None:
+        for j in range(3):
+            if put_major_value_coordinate(population) is None:
                 return new_population
             # Choose a random coordinate from population for parent1
-            parent1, parent2 = get_random_zero_coordinate(population)
+            parent1, parent2 = put_major_value_coordinate(population)
             child = reproduce(population)
-            if is_valid_child(child, parent1, parent2):
-                child = mutate(parent1, parent2, population, child)
-        return population
+            child = mutate(parent1, parent2, population, child)
+            list_machines.append((parent1, parent2))
+        return population, list_machines
+    
