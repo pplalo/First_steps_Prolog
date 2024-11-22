87a83bef8762ea85bd35d8247523250b6ea91804
pplalo <jose.eduardo.castagno@gmail.com>
2024-11-21 23:07:17 -0600

    Add variable elimination function for Bayesian network inference

---
+++ Bayes/Elimination_variables.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def eliminationAsk(X, e, bn):
+    """
+    Bayesian network inference using variable elimination.
+    :param X: The query variable
+    :param e: Observed values for variables E
+    :param bn: A Bayesian network P(X1,...,Xn)
+    :return: A distribution over X
+    """
+    factors = []
+    
+    for var in order(bn['VARS']):
+        factors.append(make_factor(var, e, bn))
+        
+        # If var is a hidden variable, sum it out
+        if var not in e and var != X:
+            factors = sum_out(var, factors)
+    
+    return normalize(pointwise_product(factors))
+
+
